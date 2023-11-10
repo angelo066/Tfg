a40f9e778739cda43b64ea3477228995608a5684
Ángel López <angelo06@ucm.es>
2023-11-10 18:55:48 +0100

    Inicio de parseo

--- Tfg/Assets/Scripts/Parsing_Scripts/JSON_Parser.cs
+++ Tfg/Assets/Scripts/Parsing_Scripts/JSON_Parser.cs
@@ -6,7 +6,7 @@ using System.IO;
 public class JSON_Parser : MonoBehaviour
 {
     //This is temporal while I try to introduce Chatgpt into the project
-    private string FileName = "\\Universidad\\tfg\\Tfg\\Tfg\\Assets\\testing_Json";
+    private string FileName = "\\Universidad\\tfg\\Tfg\\Tfg\\Assets\\testing_Json\\prueba.Json";
 
 
     int n_planets;
@@ -26,10 +26,42 @@ public class JSON_Parser : MonoBehaviour
         
     }
 
+    /*
+     * Creo que para hacer la busqueda lo mas eficiente posible
+     * lo suyo seria añadirle al mensaje que el usuario manda al chatgpt
+     * un añadido de "Estructuramelo como este ejemplo
+     */
+
     public void LoadJson(string file)
     {
-        Debug.Log(file);
-        string text = File.ReadAllText(file);
+        if (File.Exists(file))
+        {
+            StreamReader sr = new StreamReader(FileName);
+            string line;
+            line = parseNumberOfPlanets(sr);
 
+            planets = new Planet[n_planets];
+
+        }
+        else
+        {
+            Debug.LogError("Path doesn´t exist");
+        }
+        
+    }
+
+    private string parseNumberOfPlanets(StreamReader sr)
+    {
+        string line;
+        sr.ReadLine();   //We remove the first character ({)
+
+        line = sr.ReadLine();
+
+        string[] firstSplit = line.Split(":");
+        string[] secondSplit = firstSplit[1].Split(","); //Number and ,
+
+
+        n_planets = int.Parse(secondSplit[0]);
+        return line;
     }
 }
